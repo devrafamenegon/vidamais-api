8e1a0b24485584584b9f099a4cc3a2b9e74448c4
Rafael Menegon <dev.rafaelmenegon@gmail.com>
2023-06-07 01:17:05 -0300

    refactor: medic schema indentation

--- src/api/models/medic.js
+++ src/api/models/medic.js
@@ -1,16 +1,17 @@
 import mongoose from "mongoose";
 
-const MedicSchema = new mongoose.Schema({
-  name: {type: String, require: true},
-  crm: {type: String, unique: true, require: true},
-  email: {type: String, unique: true, require: true},
-  password: {type: String, require: true}
-},
-{
-  versionKey: false
-}
-)
+const MedicSchema = new mongoose.Schema(
+  {
+    name: { type: String, require: true },
+    crm: { type: String, unique: true, require: true },
+    email: { type: String, unique: true, require: true },
+    password: { type: String, require: true },
+  },
+  {
+    versionKey: false,
+  }
+);
 
-const medicSchema = mongoose.model('medics', MedicSchema)
+const medicSchema = mongoose.model("medics", MedicSchema);
 
-export default medicSchema
+export default medicSchema;
