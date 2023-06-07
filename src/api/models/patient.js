32e6c5604c9b560c30f35b690322a2492ace5d54
Rafael Menegon <dev.rafaelmenegon@gmail.com>
2023-06-07 01:15:19 -0300

    feat: improve patient schema

--- src/api/models/patient.js
+++ src/api/models/patient.js
@@ -1,16 +1,24 @@
 import mongoose from "mongoose";
+import { MedicineSchema } from "./medicine.js";
+import { ExamSchema } from "./exam.js";
+import { ConsultationSchema } from "./consultation.js";
 
-const PatientSchema = new mongoose.Schema({
-  name: {type: String, require: true},
-  cpf: {type: String, unique: true, require: true},
-  email: {type: String, unique: true, require: true},
-  password: {type: String, require: true}
-},
-{
-  versionKey: false
-}
-)
+const PatientSchema = new mongoose.Schema(
+  {
+    name: { type: String, require: true },
+    cpf: { type: String, unique: true, require: true },
+    email: { type: String, unique: true, require: true },
+    password: { type: String, require: true },
+    birthdate: { type: Date, require: true },
+    medicines: [MedicineSchema],
+    exams: [ExamSchema],
+    consultations: [ConsultationSchema],
+  },
+  {
+    versionKey: false,
+  }
+);
 
-const patientSchema = mongoose.model('patients', PatientSchema)
+const patientSchema = mongoose.model("patients", PatientSchema);
 
-export default patientSchema
+export default patientSchema;
