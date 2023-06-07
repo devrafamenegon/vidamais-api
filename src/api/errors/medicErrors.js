e55973e3bf4d0be840f4f7383bb6ff39afdefade
Rafael Menegon <dev.rafaelmenegon@gmail.com>
2023-06-07 11:03:41 -0300

    feat: improve medic errors

--- src/api/errors/medicErrors.js
+++ src/api/errors/medicErrors.js
@@ -1,1 +1,15 @@
+class MedicNotFoundError extends Error {
+  constructor() {
+    super("Medic not found");
+    this.name = "MedicNotFoundError";
+  }
+}
 
+class EmailCRMExistsError extends Error {
+  constructor() {
+    super("Email or CRM already registered");
+    this.name = "EmailCRMExistsError";
+  }
+}
+
+export { MedicNotFoundError, EmailCRMExistsError };
